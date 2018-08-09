e68d46272489255af6b6b195f7aca57ccabd3266
k.rochelle.lewis@gmail.com <k.rochelle.lewis@gmail.com>
2018-08-09 03:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+    var emptyInfo=[];
+    var names;
+    
+    
+    for (names = 0; names< musicians.length; names++) { 
+    emptyInfo.push(musicians [names] + instruments[names]);
+      
+    }
+    return emptyInfo;
+
+    
+  
